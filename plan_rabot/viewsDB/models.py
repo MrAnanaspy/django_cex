e7feae956d59248142f1ebcd730fc89e1183eb98
Stanislav <stassssssssssssss13@gmail.com>
2025-01-16 16:12:13 +0300

    Переделал автозаполнение моделей

--- plan_rabot/viewsDB/models.py
+++ plan_rabot/viewsDB/models.py
@@ -188,9 +188,9 @@ class Appeal(models.Model):
     quantity_defect = (models.IntegerField(verbose_name='Брак', null=True, blank=True, default=0))
     ready_status = MultiSelectField(max_length=50, choices=READY_STATUS, default='accept', verbose_name='Ожидаем', null=True, blank=True)
     production_status = (models.CharField(max_length=100, choices=RODUCTION_STATUS, verbose_name='Статус выполнения', null=True, blank=True))
-    AWP = (models.IntegerField(verbose_name="Средневзвешенная цена", null=True, blank=True))
-    material_price = (models.IntegerField(verbose_name='Прямые затраты на сырье, Руб', null=True, blank=True))
-    equipment_price = (models.IntegerField(verbose_name='Прямые затраты на инструмент', null=True, blank=True))
+    AWP = (models.IntegerField(verbose_name="Средневзвешенная цена", default=0))
+    material_price = (models.IntegerField(verbose_name='Прямые затраты на сырье, Руб', default=0))
+    equipment_price = (models.IntegerField(verbose_name='Прямые затраты на инструмент', default=0))
     comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
 
     def __str__(self):
