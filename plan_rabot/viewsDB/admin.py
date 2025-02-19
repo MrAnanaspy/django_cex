f5190743a5d47752c35124efd27f2fb0d3c96112
SAS <stassssssssss13@gmail.co>
2025-02-19 16:44:44 +0300

    Изменил генерацию МСК. Перенес на создание/редактирование заявки

--- plan_rabot/viewsDB/admin.py
+++ plan_rabot/viewsDB/admin.py
@@ -33,59 +33,6 @@ class AppealAdmin(admin.ModelAdmin):
     zayavka_link.short_description = 'Ссылка'
 
     def msk_link(self, obj):
-        if not os.path.isdir('media/{0}/addition'.format(obj.EAM.EAM)):
-            os.mkdir('media/{0}/addition'.format(obj.EAM.EAM))
-        config = configparser.ConfigParser()  # создаём объекта парсера
-        config.read("config.ini")
-
-        wb = openpyxl.load_workbook("media/documents/exel/MSK.xlsx")
-        sheet = wb.active
-        # № appeal
-        sheet.cell(row=10, column=2, value=obj.id)
-        # EAM
-        sheet.cell(row=10, column=3, value=obj.EAM.EAM)
-        # Name
-        sheet.cell(row=10, column=7, value=obj.EAM.name)
-        # EAM
-        sheet.cell(row=12, column=11, value=obj.EAM.size)
-        # Quantity
-        sheet.cell(row=12, column=12, value=obj.quantity)
-        # Material
-        if obj.EAM.mater:
-            sheet.cell(row=10, column=13, value=obj.EAM.mater.name)
-        # № MSK
-        sheet.cell(row=6, column=6, value="Маршрутно-сопроводительная карта (МСК) №__" + obj.start_time.strftime('%y') + str(obj.id) + '__')
-
-        dat = []
-        # Заготовительня
-        if obj.EAM.procurement_work != 0:
-            dat.append('Заготовительная')
-
-        if 'electroerosion' in obj.machine:
-            dat.append('Электроэрозионная')
-
-        if 'turning_milling' in obj.machine:
-            dat.append('Токарно-фрезерная')
-        else:
-            # Токарная
-            if 'turning' in obj.machine:
-                dat.append('Токарная')
-
-            # Фрезерная
-            if 'milling' in obj.machine:
-                dat.append('Фрезерная')
-
-
-        dat.append('Постобработка')
-        dat.append('Упаковка')
-        num = 1
-        for i in dat:
-            sheet.cell(row=15+(num * 2), column=3, value=i)
-            sheet.cell(row=15+(num * 2), column=2, value=num)
-            num+=1
-
-        wb.save('media/{0}/addition/MSK_{1}.xlsx'.format(obj.EAM.EAM, obj.EAM.EAM))
-
         return format_html('<a href="{0}">Сгенерировать МСК</a>'.format("/media/" + obj.EAM.EAM + "/addition/MSK_" + obj.EAM.EAM + ".xlsx"))
     zayavka_link.allow_tags = True
     zayavka_link.short_description = 'Ссылка'
@@ -232,6 +179,58 @@ def add_material(sender, instance, created, **kwargs):
 # Генерация TimeCost object при создании Apeal
 @receiver(post_save, sender=Appeal)
 def add_appeal(sender, instance, created, **kwargs):
+    if not os.path.isdir('media/{0}/addition'.format(instance.EAM.EAM)):
+        os.mkdir('media/{0}/addition'.format(instance.EAM.EAM))
+    config = configparser.ConfigParser()  # создаём объекта парсера
+    config.read("config.ini")
+
+    wb = openpyxl.load_workbook("media/documents/exel/MSK.xlsx")
+    sheet = wb.active
+    # № appeal
+    sheet.cell(row=10, column=2, value=instance.id)
+    # EAM
+    sheet.cell(row=10, column=3, value=instance.EAM.EAM)
+    # Name
+    sheet.cell(row=10, column=7, value=instance.EAM.name)
+    # EAM
+    sheet.cell(row=12, column=11, value=instance.EAM.size)
+    # Quantity
+    sheet.cell(row=12, column=12, value=instance.quantity)
+    # Material
+    if instance.EAM.mater:
+        sheet.cell(row=10, column=13, value=instance.EAM.mater.name)
+    # № MSK
+    sheet.cell(row=6, column=6,
+               value="Маршрутно-сопроводительная карта (МСК) №__" + instance.start_time.strftime('%y') + str(instance.id) + '__')
+
+    dat = []
+    # Заготовительня
+    if instance.EAM.procurement_work != 0:
+        dat.append('Заготовительная')
+
+    if 'electroerosion' in instance.machine:
+        dat.append('Электроэрозионная')
+
+    if 'turning_milling' in instance.machine:
+        dat.append('Токарно-фрезерная')
+    else:
+        # Токарная
+        if 'turning' in instance.machine:
+            dat.append('Токарная')
+
+        # Фрезерная
+        if 'milling' in instance.machine:
+            dat.append('Фрезерная')
+
+    dat.append('Постобработка')
+    dat.append('Упаковка')
+    num = 1
+    for i in dat:
+        sheet.cell(row=15 + (num * 2), column=3, value=i)
+        sheet.cell(row=15 + (num * 2), column=2, value=num)
+        num += 1
+
+    wb.save('media/{0}/addition/MSK_{1}.xlsx'.format(instance.EAM.EAM, instance.EAM.EAM))
     if created:
         '''TimeCosts.objects.create(
                                 appeal_id_id = instance.id,
